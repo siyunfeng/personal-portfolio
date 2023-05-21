d043d3688432cc213f1af6de58752856096ae367
Siyun Feng <siyunf21@gmail.com>
2023-05-21 01:23:20 -0400

    removed unused elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -64,17 +64,6 @@ const NavBar = () => {
             >
               Projects
             </Nav.Link>
-            {/* <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
