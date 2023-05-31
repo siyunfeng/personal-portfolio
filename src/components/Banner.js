ac5b666d3284cb36f47c6bc6c260d6c2994c33cd
Siyun Feng <siyunf21@gmail.com>
2023-05-30 23:53:06 -0400

    updated home page intro

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,11 +10,17 @@ const Banner = () => {
             <h1>{`Hi, I'm Siyun.`}</h1>
             <h4 className='wrap'>Full Stack Developer</h4>
             <p>
-              I recently graduated from Fullstack Academy as a full-stack
-              engineer. My first programming language is JavaScript. I enjoy
-              teamwork and building applications. My 6 years of HR and customer
-              service experience allowed me to develop excellent communication,
-              interpersonal, and problem-solving skills.
+              As a JavaScript developer, I possess a unique blend of creativity,
+              teamwork, and rapid learning abilities. My passion lies in
+              designing applications that prioritize the user experience and
+              allow me to showcase my creativity.
+            </p>
+            <p>
+              Drawing from 6 years of HR and customer service experience, my
+              communication, interpersonal skills, and problem-solving abilities
+              allow me to thrive in collaborative environments. I am dedicated
+              to continuously improving my skills and making valuable
+              contributions to impactful software development projects.
             </p>
             <a href='https://www.linkedin.com/in/siyunfeng/' target={`_blank`}>
               <button className='banner-connect-button'>Let's connect</button>
